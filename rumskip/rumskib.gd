7efe93b11626ddf37cf6f0f4dbf60dcc5b147347
simen <simenhenrikaen@gmail.com>
2026-02-16 10:40:43 +0100

    arbejder stadig på player

--- rumskip/rumskib.gd
+++ rumskip/rumskib.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
-const Gravity = 1
+var Sidethrust = 0
+var Thrust = 0
+const Gravity = 10
 
 func _ready() -> void:
 	velocity.x += randf_range(-50,60)
 
 func _physics_process(delta: float) -> void:
-	velocity.y += Gravity 
+	velocity.y += delta * Gravity
+	velocity += Vector2(0,-Thrust)
 	move_and_slide()
 	
+	if Input.is_action_just_pressed("Thrust"):
+		Thrust += 10
+	if Input.is_action_just_pressed("Dethrust") and Thrust >= 0:
+		Thrust -= 10
+	if Input.is_action_just_pressed("Rightthrust"):
+		rotation_degrees += 10
