3a28f830f04e01a367286d1e421cd0752e1e60b5
filip <filipskydt@gmail.com>
2026-02-17 12:40:08 +0100

    en masse små ting

--- rumskip/menu.gd
+++ rumskip/menu.gd
@@ -1,13 +1,7 @@
 extends Control
 
-
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	pass
-
 
 func _on_info_pressed() -> void:
-	get_tree().change_scene_to_file("res://Info.tscn")
-	pass 
+	get_tree().change_scene_to_file("res://Info.tscn") 
