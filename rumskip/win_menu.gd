d1af337e044cf5f14dbab669d7241a5285314275
filip <filipskydt@gmail.com>
2026-02-23 09:23:38 +0100

    lavet win/lose menu

---
+++ rumskip/win_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_back_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_go_agen_pressed() -> void:
+	get_tree().change_scene_to_file("res://moon an starsky.tscn")
