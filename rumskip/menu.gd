b8ecd0fc2b1b09d60d1f64f662d2ca5d8d736faf
filip <filipskydt@gmail.com>
2026-02-23 08:35:09 +0100

    har lavet colition til månen

--- rumskip/menu.gd
+++ rumskip/menu.gd
@@ -5,3 +5,9 @@ func _on_quit_pressed() -> void:
 
 func _on_info_pressed() -> void:
 	get_tree().change_scene_to_file("res://Info.tscn") 
+
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://moon an starsky.tscn")
